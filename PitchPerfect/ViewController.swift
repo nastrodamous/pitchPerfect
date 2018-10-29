97d9e7693bcc79bfc34249ff6da341a2f44aefd4
Andre Davis <andre@andres-macbook-pro.local>
2018-10-29 19:10:56 -0400

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Andre Davis on 10/29/18.
+//  Copyright © 2018 Andre Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
